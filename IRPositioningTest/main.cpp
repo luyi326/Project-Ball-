830d688a7929cc1dd8663592cd249b149ad18a7c
Yi Lu <luyi326@gmail.com>
2015-04-17 22:06:10 -0700

    Revert "new IR test"
    This reverts commit e2c82603ed01914a414530f3a5df5e300d46ada9.

--- IRPositioningTest/main.cpp
+++ IRPositioningTest/main.cpp
@@ -9,11 +9,12 @@ using namespace std;
 using namespace BlackLib;
 
 int main (int argc, char* argv[]) {
-	IRRim rim(4, EHRPWM1B, GPIO_48, AIN0);
+	IRRim rim(2, EHRPWM1B, GPIO_48, AIN0);
 	cout << std::dec;
 
 	while (1) {
         rim.run();
+		usleep(5000);
 	}
 	return 0;
 }
